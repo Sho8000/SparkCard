ed71bd99b6119139270bb3c2675fbe9767a8d4d1
Sho8000 <yoshikichi8000@gmail.com>
2025-02-27 00:32:14 -0800

    first commit

---
+++ src/app/components/Cards/OnlyTextList.tsx
@@ -0,0 +1,39 @@
+import Image, { StaticImageData } from "next/image";
+import "./CardStyle.css"
+import sample from "../../../../public/globe.svg"
+
+interface TextList {
+  explain:string;
+  imageUrl:StaticImageData;
+  listItem:string[];
+}
+
+interface DataProps{
+  data:TextList
+}
+
+export default function OnlyTextList(
+  {data}:DataProps
+) {
+  const {explain,imageUrl,listItem} = data;
+  return (
+    <>
+      <div className="OnlyTextListContainer h-auto basis-1/2 py-[2rem] px-[1rem] ">
+        <h2 className="text-lg mt-[1rem]">{explain}</h2>
+        <ul className="text-base mt-[1rem]">
+          {listItem.map((item, index)=>
+            <li className="onlyTextList" key={`onlyText_${index}`}> {item}</li>
+          )}
+        </ul>
+      </div>
+      <div className="h-auto basis-1/2 py-[2rem] px-[1rem] relative"> 
+        <Image
+          src={imageUrl}
+          alt="Img"
+          width={300}
+          style={{margin:"auto"}}
+        />
+      </div>
+    </>
+  );
+}
